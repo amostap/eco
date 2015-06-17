442ac0357bc5a0488b42a8439bff91d51d3ecbd8
tyfoan <myonlygamess@gmail.com>
2015-06-18 00:14:11 +0300

    Context

---
+++ eco_design/eco_design/DAL/CompanyContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eco_design.DAL
+{
+    public class CompanyContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ eco_design/eco_design/DAL/Pruduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eco_design.DAL
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int VolumeOfProduction { get; set; } 
+        public int Demand { get; set; } //спрос N
+        public int CostOfProduct { get; set; } //стоимость C
+        public virtual Company Company { get; set; }
+
+    }
+}
