2e2940f8e1f50781c67ecced4c7ad0ddff1c1a46
Oleksii Ponomarenko <Oleksii_Ponomarenko@epam.com>
2015-06-20 17:12:24 +0300

    Try to draw.

--- eco_design/eco_design/UI/MainWindow.xaml.cs
+++ eco_design/eco_design/UI/MainWindow.xaml.cs
@@ -11,8 +11,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HelixToolkit.Wpf;
 
 namespace eco_design
 {
@@ -25,9 +27,92 @@ namespace eco_design
         {
             InitializeComponent();
 
-            CompanyContext c = new CompanyContext();
-            c.Companies.Add(new Company() { CompanyName = "1" });
-            c.SaveChanges();
+            //HelixViewport3D.ResetCamera();
+
+            //HelixViewport3D.Camera = new PerspectiveCamera
+            //{
+            //    Position = new Point3D(100, 100, 100),
+            //    LookDirection = new Vector3D(-100, -100, -100),
+            //    UpDirection = new Vector3D(0, 0, 1)
+            //};
+
+            TubeVisual3D tube = new TubeVisual3D();
+            tube.Path = new Point3DCollection
+            {
+                new Point3D(0, 0, 0), 
+                new Point3D(50, 0, 0)
+            };
+            tube.Diameter = 0.1;
+            tube.Fill = Brushes.Red;
+            tube.IsPathClosed = false;
+
+            HelixViewport3D.Children.Add(tube);
+            
+            tube = new TubeVisual3D();
+            tube.Path = new Point3DCollection
+            {
+                new Point3D(0, 0, 0), 
+                new Point3D(0, 50, 0)
+            };
+            tube.Diameter = 0.1;
+            tube.Fill = Brushes.Blue;
+            tube.IsPathClosed = false;
+
+            HelixViewport3D.Children.Add(tube);
+
+            tube = new TubeVisual3D();
+            tube.Path = new Point3DCollection
+            {
+                new Point3D(0, 0, 0), 
+                new Point3D(0, 0, 50)
+            };
+            tube.Diameter = 0.1;
+            tube.Fill = Brushes.Green;
+            tube.IsPathClosed = false;
+
+            HelixViewport3D.Children.Add(tube);
+
+            for (int i = 0; i <= 50; i++)
+            {
+                tube = new TubeVisual3D();
+                tube.Path = new Point3DCollection
+                {
+                    new Point3D(0, 0, 0), 
+                    new Point3D(0, 0, 50)
+                };
+
+                tube.Diameter = 0.1;
+                tube.Fill = Brushes.DarkGray;
+                tube.IsPathClosed = false;
+
+                HelixViewport3D.Children.Add(tube);
+
+                tube = new TubeVisual3D();
+                tube.Path = new Point3DCollection
+                {
+                    new Point3D(0, 0, 0), 
+                    new Point3D(0, 0, 50)
+                };
+
+                tube.Diameter = 0.1;
+                tube.Fill = Brushes.DarkGray;
+                tube.IsPathClosed = false;
+
+                HelixViewport3D.Children.Add(tube);
+
+                tube = new TubeVisual3D();
+                tube.Path = new Point3DCollection
+                {
+                    new Point3D(0, 0, 0), 
+                    new Point3D(0, 0, 50)
+                };
+
+                tube.Diameter = 0.1;
+                tube.Fill = Brushes.DarkGray;
+                tube.IsPathClosed = false;
+
+                HelixViewport3D.Children.Add(tube);
+            }
         }
     }
 }
